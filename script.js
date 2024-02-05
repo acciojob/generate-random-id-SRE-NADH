94de0a41811bc78df17d5bfd2bde257f72f459a6
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2024-02-06 02:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function makeid(l) {
   // write your code here
 	let tmpString = abcdefghijklmnopqrstuvwxyz0123456789;
 	let string = "";
-	for( let i=0;i<tmpString.length;i++){
+	for( let i=0;i<l;i++){
 		let index = Math.floor(Marh.random()*(tmpString.length-1));
 		string+=tmpString[index];
 	}
 	return string;
 }
 
-
-// Do not change the code below.
+// Do not change the code below
 const l = prompt("Enter a number.");
 alert(makeid(l));
