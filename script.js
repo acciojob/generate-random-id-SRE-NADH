ff6d04d38f6cf59305c941d4443f1d4362c5d51c
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2024-02-05 20:43:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let tmpString = abcdefghijklmnopqrstuvwxyz0123456789;
+	let string = "";
+	for( let i=0;i<tmpString.length;i++){
+		let index = Math.floor(Marh.random()*(tmpString.length-1));
+		string+=tmpString[index];
+	}
+	retur string;
 }
 
 // Do not change the code below.
